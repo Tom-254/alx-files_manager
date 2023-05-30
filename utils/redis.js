f0f73add5aad98b984abe7ef594680cfa6828cec
Tish254 <tiegomseeraoscar295@gmail.com>
2023-05-31 02:08:32 +0300

    0x04. Files manager

---
+++ utils/redis.js
@@ -0,0 +1,54 @@
+import { promisify } from 'util';
+import { createClient } from 'redis';
+
+/**
+ * Class Defining necessary properties and methods of a Redis client.
+ */
+class RedisClient {
+  /**
+   * Creates a RedisClient instance.
+   */
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
+
+  /**
+   * Checks Connections Active Status.
+   */
+  isAlive() {
+    return this.isClientConnected;
+  }
+
+  /**
+   * Gets the value of the passed key.
+   */
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
+
+  /**
+   * Stores the value of the passed key.
+   */
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX)
+      .bind(this.client)(key, duration, value);
+  }
+
+  /**
+   * Removes the Value of the passed key.
+   */
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+
+export const redisClient = new RedisClient();
+export default redisClient;
